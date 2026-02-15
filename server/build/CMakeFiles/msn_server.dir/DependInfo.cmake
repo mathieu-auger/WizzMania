2e88a4c791295cb5589fb1b576617000c19633d8
daria-marchenko <daria.marchenko@laplateforme.io>
2026-02-15 17:30:29 +0100

    Add connection with server and client

---
+++ server/build/CMakeFiles/msn_server.dir/DependInfo.cmake
@@ -0,0 +1,24 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "C:/Users/daria/Documents/2_anee/c++/wizz_mania/WizzMania/server/main.cpp" "CMakeFiles/msn_server.dir/main.cpp.obj" "gcc" "CMakeFiles/msn_server.dir/main.cpp.obj.d"
+  "C:/Users/daria/Documents/2_anee/c++/wizz_mania/WizzMania/server/routes.cpp" "CMakeFiles/msn_server.dir/routes.cpp.obj" "gcc" "CMakeFiles/msn_server.dir/routes.cpp.obj.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ server/build/CMakeFiles/msn_server.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/msn_server.dir/main.cpp.obj"
+  "CMakeFiles/msn_server.dir/main.cpp.obj.d"
+  "CMakeFiles/msn_server.dir/routes.cpp.obj"
+  "CMakeFiles/msn_server.dir/routes.cpp.obj.d"
+  "libmsn_server.dll.a"
+  "msn_server.exe"
+  "msn_server.exe.manifest"
+  "msn_server.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/msn_server.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
