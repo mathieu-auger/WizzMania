cbd4d706bd582113f62e26fdc99a183f05eeddd2
mathieu-auger <mathieu.auger@laplateforme.io>
2026-03-23 11:36:40 +0100

    final version

---
+++ build/server/CMakeFiles/msn_server.dir/DependInfo.cmake
@@ -0,0 +1,25 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "C:/Users/Mathieu Auger/Documents/Projets 2eme annee/WizZMania/server/database.cpp" "server/CMakeFiles/msn_server.dir/database.cpp.obj" "gcc" "server/CMakeFiles/msn_server.dir/database.cpp.obj.d"
+  "C:/Users/Mathieu Auger/Documents/Projets 2eme annee/WizZMania/server/main.cpp" "server/CMakeFiles/msn_server.dir/main.cpp.obj" "gcc" "server/CMakeFiles/msn_server.dir/main.cpp.obj.d"
+  "C:/Users/Mathieu Auger/Documents/Projets 2eme annee/WizZMania/server/routes.cpp" "server/CMakeFiles/msn_server.dir/routes.cpp.obj" "gcc" "server/CMakeFiles/msn_server.dir/routes.cpp.obj.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/server/CMakeFiles/msn_server.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for msn_server.
+# This may be replaced when dependencies are built.
